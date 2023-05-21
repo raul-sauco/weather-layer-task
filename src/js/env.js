b2d178880b5dd9e5b9b0094b3ee553dd200b278e
Raul Sauco <sauco.raul@gmail.com>
2023-05-21 17:12:17 +0200

    Created weather widget

--- src/js/env.js
+++ src/js/env.js
@@ -1,3 +1,7 @@
+// This file simulates data that would be coming from the database and/or
+// environmental variables.
 // Short lived keys, publishing to GitHub is OK, will be revoked shortly.
 export const API_KEY = "31858926077f886e74907b01c1a2b912";
 export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+export const REFRESH_UI_INTERVAL = 2000;
+export const REFRESH_DATA_INTERVAL = 10 * 60 * 1000;
--- src/js/site.js
+++ src/js/site.js
@@ -1,7 +1,56 @@
-import { API_KEY, BASE_URL } from "./env.js";
+import {
+  API_KEY,
+  BASE_URL,
+  REFRESH_DATA_INTERVAL,
+  REFRESH_UI_INTERVAL,
+} from "./env.js";
 
-console.debug("Hello Layer");
+// This constant holds our application state.
+const WL = {
+  fetchDataInterval: null,
+  refreshUIInterval: null,
+  currentData: null,
+  currentPosition: "topLeft",
+  endpoint: `${BASE_URL}?q=London&appid=${API_KEY}`,
+  container: null,
+};
 
-// Use the constants
-console.log(API_KEY);
-console.log(BASE_URL);
+const main = () => {
+  // Call right now, then set interval.
+  WL.container = document.getElementById("weather-layer-container");
+  fetchAPIData(WL.endpoint);
+  WL.fetchDataInterval = setInterval(() => {
+    fetchAPIData(WL.endpoint);
+  }, REFRESH_DATA_INTERVAL);
+  refreshUI();
+  WL.refreshUIInterval = setInterval(refreshUI, REFRESH_UI_INTERVAL);
+};
+
+/**
+ * A function that fetches data from a given URL.
+ * @param {string} url
+ */
+async function fetchAPIData(url) {
+  console.debug(`Fetching data from ${url}`);
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    WL.currentData = json;
+    console.debug(json);
+  } catch (e) {
+    console.warn("Fetch or parse error", e);
+  }
+}
+
+/**
+ * A function that refreshes the user interface.
+ */
+function refreshUI() {
+  console.debug("Refreshing the user interface");
+  const currentClass = WL.container.classList[0];
+  const position = +currentClass.match(/\d+/)[0];
+  WL.container.classList.remove(currentClass);
+  WL.container.classList.add(`pos-${(position + 1) % 4}`);
+}
+// Call the main function.
+main();
