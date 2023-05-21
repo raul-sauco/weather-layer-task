a95d85ca21701c990d6b1b98897d827f258030f7
Raul Sauco <sauco.raul@gmail.com>
2023-05-21 16:11:58 +0200

    Configure modules

---
+++ src/js/env.js
@@ -0,0 +1,3 @@
+// Short lived keys, publishing to GitHub is OK, will be revoked shortly.
+export const API_KEY = "31858926077f886e74907b01c1a2b912";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
--- src/js/site.js
+++ src/js/site.js
@@ -1,1 +1,7 @@
+import { API_KEY, BASE_URL } from "./env.js";
+
 console.debug("Hello Layer");
+
+// Use the constants
+console.log(API_KEY);
+console.log(BASE_URL);
